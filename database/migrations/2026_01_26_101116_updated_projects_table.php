21db256ab67a4531430da59f65bfc209a26a223e
Mickhail-Kolupaichenkov <kolupaichenkov@gmail.com>
2026-01-26 15:16:02 +0500

    Добавил поля ответственного и дедлайн, через новую миграцию

---
+++ database/migrations/2026_01_26_101116_updated_projects_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->foreignId('assignee_id')->constrained('users');
+            $table->date('deadline_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropForeign(['assignee_id']);
+
+            $table->dropColumn('deadline_date');
+            $table->dropColumn('assignee_id');
+        });
+    }
+};
